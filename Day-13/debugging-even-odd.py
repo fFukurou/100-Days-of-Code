92e18bee7ce4a3645ce629ad27f8d75334c9e049
fFukurou <supevi1@gmail.com>
2024-05-31 23:36:37 -0300

    Day 13

---
+++ Day-13/debugging-even-odd.py
@@ -0,0 +1,17 @@
+#BUGGED
+""" number = int(input()) # Which number do you want to check?
+
+if number % 2 = 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.") """
+  
+
+#DEBUGGED
+number = int(input()) # Which number do you want to check?
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+  
---
+++ Day-13/debugging-leap-year.py
@@ -0,0 +1,29 @@
+#BUGGED
+""" # Which year do you want to check?
+year = input()
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.") """
+
+#DEBUGGED
+# Which year do you want to check?
+year = int(input())
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
